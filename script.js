24b51a73c1697b77b16f7f7afd76bee3b34cfd20
thainara2612 <146358715+thainara2612@users.noreply.github.com>
2024-08-13 16:39:01 -0300

    dd

---
+++ script.js
@@ -0,0 +1,22 @@
+const caixaPrincipal = document.querySelector(".caixa-principal");
+const caixaPerguntas = document.querySelector(".caixa-perguntas");
+const caixaAlternativas = document.querySelector(".caixa-alternativas");
+const caixaResultados = document.querySelector(".caixa-resultado");
+const textoResultado = document.querySelector(".texto-resultado");
+
+const perguntas = [
+    {
+        enunciado:"Pergunta 1",
+        alternativa:[
+            "Alternativa 1",
+            "Alternativa 2"
+        ]
+    },
+    {
+        enunciado: "pergunta 2",
+        alternativa:[
+            "Alternativa 1",
+            "Alternativa 2"
+        ]
+    }
+];
